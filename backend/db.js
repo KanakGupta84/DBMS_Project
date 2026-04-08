9d43f9cb2d572357ea9001b3a3cd21bee87b83f7
mridul <mridulvermadell@gmail.com>
2026-04-09 00:51:07 +0530

    added the req features

--- backend/db.js
+++ backend/db.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'SQLpassword1.', // Add your MySQL password here
+  user: process.env.DB_USER || 'mridul',
+  password: process.env.DB_PASSWORD || '1991', // Add your MySQL password here
   database: process.env.DB_NAME || 'dbms_project',
   waitForConnections: true,
   connectionLimit: 10,
